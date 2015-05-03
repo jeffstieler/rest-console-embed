1c4970b61e30be0e2c520f8ff68e3bfb5d1d31e0
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-05-03 10:27:22 -0600

    Properly set task dependencies to avoid build errors.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-	execSync = require('child_process').execSync,
+	exec = require('child_process').exec,
 	rename = require('gulp-rename');
 
 var consoleDir = 'vendor/jeffstieler/rest-api-console2';
@@ -8,21 +8,31 @@ gulp
 
 .task('default', ['console-npm-install', 'console-copy-plugin-config', 'console-build', 'console-copy-build'])
 
-.task('console-npm-install', function() {
-	execSync('npm install', { 'cwd': consoleDir });
+.task('console-npm-install', function(cb) {
+	exec('npm install', { 'cwd': consoleDir }, function(err) {
+		if (err) {
+			return cb(err);
+		}
+		cb();
+	});
 })
 
 .task('console-copy-plugin-config', function() {
-	gulp.src('config.plugin.json')
+	return gulp.src('config.plugin.json')
 		.pipe(rename('config.json'))
 		.pipe(gulp.dest(consoleDir));
 })
 
-.task('console-build', function(cb) {
-	execSync('npm run build', { 'cwd': consoleDir });
+.task('console-build', ['console-copy-plugin-config', 'console-npm-install'], function(cb) {
+	exec('npm run build', { 'cwd': consoleDir }, function(err) {
+		if (err) {
+			return cb(err);
+		}
+		cb();
+	});
 })
 
-.task('console-copy-build', function() {
+.task('console-copy-build', ['console-build'], function() {
 	gulp.src(consoleDir + '/build/dist/wpcom-console/public/**')
 		.pipe(gulp.dest('console'));
 });
